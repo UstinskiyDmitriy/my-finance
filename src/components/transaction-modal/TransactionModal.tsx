7a9da63f1674cf7129bc98c96c21565d0410d6e4
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2025-02-07 11:42:24 +0300

    refactoring
    aggregat transactions by categories

--- src/components/transaction-modal/TransactionModal.tsx
+++ src/components/transaction-modal/TransactionModal.tsx
@@ -1,28 +1,34 @@
 import styles from './TransactionModal.module.css';
-import { Transaction } from '../../features/transactions/types';
+import { useTransactions } from '../../features/transactions/hooks';
 
 interface TransactionModalProps {
   type: 'income' | 'expense';
-  transactions: Transaction[];
   onClose: () => void;
 }
 
-export default function TransactionModal({ type, transactions, onClose }: TransactionModalProps) {
+export default function TransactionModal({ type, onClose }: TransactionModalProps) {
+  const { incomeByCategory, expenseByCategory } = useTransactions();
+
+  const dataByCategory = type === 'income' ? incomeByCategory : expenseByCategory;
+
   return (
     <div className={styles.overlay} onClick={onClose}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
         <h2>{type === 'income' ? 'Доходы' : 'Расходы'}</h2>
         <button className={styles.closeButton} onClick={onClose}>×</button>
-        <ul className={styles.list}>
-          {transactions.length === 0 ? <p>{type === 'income' ? "Нет статей доходов" : "Нет статей расходов"}</p> : transactions.map((t) => (
-            <li key={t.id} className={styles.transaction}>
-              <span>{t.category}</span>
-              <span>{t.amount.toLocaleString()} ₽</span>
-              <span>{new Date(t.date).toLocaleDateString()}</span>
-            </li>
-          ))}
-          
-        </ul>
+
+        {Object.keys(dataByCategory).length === 0 ? (
+          <p>{type === 'income' ? "Нет статей доходов" : "Нет статей расходов"}</p>
+        ) : (
+          <ul className={styles.list}>
+            {Object.entries(dataByCategory).map(([category, amount]) => (
+              <li key={category} className={styles.transaction}>
+                <span>{category}</span>
+                <span>{amount.toLocaleString()} ₽</span>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
