7ebdcdcc4514b1c0328075d0c49a360dce511c48
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2025-02-08 12:08:17 +0300

    small changes

--- src/components/comprasion/Comprasion.tsx
+++ src/components/comprasion/Comprasion.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react';
-import { useTransactions } from '../../features/transactions/hooks';
-import styles from './Comprasion.module.css';
-import TransactionModal from '../transaction-modal/TransactionModal';
+import { useState } from "react";
+import { useTransactions } from "../../features/transactions/hooks";
+import styles from "./Comprasion.module.css";
+import TransactionModal from "../transaction-modal/TransactionModal";
 export default function Comprasion() {
+  const { incomeTotal, expenseTotal } = useTransactions();
+  const [modalType, setModalType] = useState<"income" | "expense" | null>(null);
+  const openModal = (type: "income" | "expense") => () => {
+    setModalType(type);
+  };
 
-    const {incomeTotal, expenseTotal} = useTransactions();
-    const [modalType, setModalType] = useState<'income' | 'expense' | null>(null);
-      const openModal = (type: 'income' | 'expense') =>() => {
-        setModalType(type)
-    }
-
-    const closeModal = () => {
-        setModalType(null)
-    }
+  const closeModal = () => {
+    setModalType(null);
+  };
 
   return (
     <div className={styles.comparison_container}>
-      <div className={styles.column} onClick={openModal('income')}>
+      <div className={styles.column} onClick={openModal("income")}>
         <h3 className={styles.title}>Доходы</h3>
-        <div className={`${styles.screen} ${styles.income}`}>{incomeTotal.toLocaleString()} ₽</div>
+        <div className={`${styles.screen} ${styles.income}`}>
+          {incomeTotal.toLocaleString()} ₽
+        </div>
       </div>
-      <div className={`${styles.column}`} onClick={(openModal('expense'))}>
+      <div className={`${styles.column}`} onClick={openModal("expense")}>
         <h3 className={styles.title}>Расходы</h3>
-        <div className={`${styles.screen} ${styles.expense}`}>{expenseTotal.toLocaleString()} ₽</div>
+        <div className={`${styles.screen} ${styles.expense}`}>
+          {expenseTotal.toLocaleString()} ₽
+        </div>
       </div>
 
-      {modalType && (
-        <TransactionModal 
-                  type={modalType}
-                  onClose={closeModal}        />
-      )}
+      {modalType && <TransactionModal type={modalType} onClose={closeModal} />}
     </div>
   );
 }
