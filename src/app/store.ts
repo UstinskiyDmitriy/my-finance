13201098b70515113a4270642139fedbcc4d9467
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2025-02-20 13:10:11 +0300

    removed persistor

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,38 +1,14 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import transactionsReducer from '../features/transactions/slice';
+import transactionsReducer from '../features/slices/transactionSlice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['transactions'], 
-    version: 1,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    migrate: (state: any) => {
-    
-      return Promise.resolve(state);
-    },
-  };
 
 const rootReducer = combineReducers({
     transactions: transactionsReducer,
-  });
-  
-  const persistedReducer = persistReducer(persistConfig, rootReducer); 
-  
-  export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-  });
+});
 
-export const persistor = persistStore(store);
+export const store = configureStore({
+    reducer: rootReducer,
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
