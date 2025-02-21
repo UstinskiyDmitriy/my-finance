ad384dc46b2e552947ad8a25e661752921c2451e
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2025-02-21 12:44:15 +0300

    added transaction with user_id

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from "../app/store";
 import { selectExpenseByCategory, selectExpenseTotal, selectIncomeByCategory, selectIncomeTotal } from "../features/selectors/TransactionSelectors";
 import { loadTransactions, createTransaction, removeTransaction, editTransaction } from "../features/slices/transactionSlice";
 import { Transaction} from "../types/types";
+import { supabase } from "../app/supabase";
 
 export const useTransactions = () => {
   const dispatch = useAppDispatch();
@@ -20,17 +21,24 @@ export const useTransactions = () => {
 
 
   const handleAddTransaction = async (values: Transaction) => {
-    const newTransaction = {
-      ...values,
-      date: new Date().toISOString(),
-    };
-
     try {
-      await dispatch(createTransaction(newTransaction)).unwrap(); 
+      const { data: userData, error: userError } = await supabase.auth.getUser();
+      if (userError || !userData.user) {
+        throw new Error("Пользователь не авторизован");
+      }
+  
+      const newTransaction = {
+        ...values,
+        date: new Date().toISOString(),
+        user_id: userData.user.id,
+      };
+  
+      await dispatch(createTransaction(newTransaction)).unwrap();
     } catch (error) {
       console.error("Ошибка добавления транзакции:", error);
     }
   };
+  
 
 
   const handleDeleteTransaction = async (id: string) => {
