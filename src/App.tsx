bbc776d56114a2a3efd7c8c50f47f05d4d67c79b
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2025-02-21 12:45:05 +0300

    added route for auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import styles from './App.module.css';
-import MainPage from './pages/main/MainPage';
-import NavBar from './components/nav-bar/NavBar';
-import ChartPage from './pages/chart/ChartPage';
-import HistoryPage from './pages/history/HistoryPage';
-import Savings from './pages/savings/Savings';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+import MainPage from "./pages/main/MainPage";
+import NavBar from "./components/nav-bar/NavBar";
+import ChartPage from "./pages/chart/ChartPage";
+import HistoryPage from "./pages/history/HistoryPage";
+import Savings from "./pages/savings/Savings";
+import { supabase } from "./app/supabase";
+import AuthPage from "./pages/auth-page/AuthPage";
 
 export default function App() {
-  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      if (data.user) setUser(data.user);
+    };
+    getUser();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
   return (
     <Router>
       <div className={styles.container}>
-       <NavBar />
+        {user && <NavBar />}
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/charts" element={<ChartPage/>} />
-          <Route path="/history" element={<HistoryPage/>} />
-          <Route path="/savings" element={<Savings />} />
+          {user ? (
+            <>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/charts" element={<ChartPage />} />
+              <Route path="/history" element={<HistoryPage />} />
+              <Route path="/savings" element={<Savings />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="*" element={<Navigate to="/auth" />} />
+            </>
+          )}
         </Routes>
       </div>
     </Router>
   );
 }
+
